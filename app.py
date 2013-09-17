4e3488e7390bad4e5b3cd9f29ef77e26caebf3f7
joyjding <joy.j.ding@gmail.com>
2013-09-17 15:20:09 -0700

    Ch 3 - Simple Voicemail

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from flask import render_template
 from twilio import twiml
 from twilio.rest import TwilioRestClient
 
+recording_url = None
+
 # Pull in configuration from system environment variables
 TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
@@ -19,18 +21,22 @@ client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 # Create a Flask web app
 app = Flask(__name__)
 
-# Render the home page
 @app.route('/', methods=['GET', 'POST'])
 def hello_you():
     resp = twiml.Response()
-    resp.say("To hear some generated music, press 1. Otherwise, press 2")
+    resp.say("To hear some generated music, press 1. Otherwise, press 2. To record a message, press 3. To listen to the previous message, press 4.")
     resp.gather(numDigits=1, action="handle-key", method="POST")
     return str(resp)
 
 @app.route("/handle-key", methods=['GET', 'POST'])     
 def handle_key():
     digit_pressed = request.values.get('Digits', None)
-    if digit_pressed == "1":
+    if digit_pressed == "0":
+        resp = twiml.Response()
+        resp.dial("6174609720")
+        resp.say("Connecting you to the master human")
+        return str(resp)
+    elif digit_pressed == "1":
         resp = twiml.Response()
         resp.say("la le lolo la loo hey hey")
         return str(resp)
@@ -38,8 +44,29 @@ def handle_key():
         resp = twiml.Response()
         resp.say("Why didn't you want to hear the music?")
         return str(resp)
+    elif digit_pressed == "3":
+        resp = twiml.Response()
+        resp.say("Record yourself. Less than 15 seconds of glory, please.")
+        resp.record(maxLength="15", action="/handle-recording")
+        resp.say("Great.")
+        return str(resp)
+    elif digit_pressed =="4":
+        resp = twiml.Response()
+        resp.play(recording_url)
+        return str(resp)
     else:
         return redirect("/")
+
+@app.route('/handle-recording', methods=['GET', 'POST'])
+def handle_recording():
+    global recording_url
+    recording_url = request.values.get("RecordingUrl", None)
+
+    resp = twiml.Response()
+    resp.say("You're famous already! Listen to your previous self.")
+    resp.play(recording_url)
+    resp.say("Nice, right?")
+    return str(resp)
 
 @app.route('/message', methods=['POST'])
 # Handle a POST request to send a text message. This is called via ajax
